e83726c65c6655b7ba9d58e285d0502a41303c14
TysonJay00 <twilde@byupathway.edu>
2026-04-16 11:12:24 -0600

    initial commit

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -1,1 +1,65 @@
-document.getElementById("lastModified").innerHTML = document.lastModified;
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("lastModified").textContent = document.lastModified;
+
+    const players = [
+        { name: "Wayne Gretzky" },
+        { name: "Connor Mcdavid" },
+        { name: "Leon Draisaitl" },
+        { name: "Mark Messier" },
+        { name: "Grant Fuhr" },
+        { name: "Ryan Smith" },
+        { name: "Paul Coffey" },
+        { name: "Dwayne Roloson"}
+    ];
+
+    const select = document.querySelector('#playerSelect');
+    const button = document.querySelector('#save');
+    const output = document.querySelector('#output');
+
+    function loadPlayers() {
+        select.innerHTML = `<option value="">--choose a player--</option>`;
+        players.forEach(player => {
+            select.innerHTML += `<option value="${player.name}">${player.name}</option>`;
+        });
+    }
+
+    function saveFavourite() {
+        const picked = select.value;
+
+        if (picked === "") {
+            output.textContent = "Please select a player.";
+            return;
+        }
+
+        localStorage.setItem('favouritePlayer', picked);
+        displayFavourite();
+    }
+
+    function displayFavourite() {
+        const fav = localStorage.getItem("favouritePlayer");
+
+        if (!fav) {
+            output.textContent = "No player has been selected yet";
+            return;
+        }
+
+        select.value = fav;
+
+        const player = players.find(p => p.name === fav);
+
+        if (player) {
+            output.innerHTML = `Your favourite player is <strong>${player.name}</strong>`;
+        } else {
+            output.textContent = "Saved player not found";
+        }
+    }
+
+    button.addEventListener("click", saveFavourite);
+
+    loadPlayers();
+    displayFavourite();
+
+});
+
+
